7c17fb72fd0a7ca23de502194d045cbbb9d64d89
SwapnaliWagh <102720948+SwapnaliWagh@users.noreply.github.com>
2024-12-01 20:24:23 +0530

    Update Fraud detection data.sql

--- Fraud detection data.sql
+++ Fraud detection data.sql
@@ -1,16 +1,3 @@
-Enter password: ********
-Welcome to the MySQL monitor.  Commands end with ; or \g.
-Your MySQL connection id is 21
-Server version: 9.1.0 MySQL Community Server - GPL
-
-Copyright (c) 2000, 2024, Oracle and/or its affiliates.
-
-Oracle is a registered trademark of Oracle Corporation and/or its
-affiliates. Other names may be trademarks of their respective
-owners.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
 mysql> create database Fraud_Detection_Data_analysis;
 ERROR 1007 (HY000): Can't create database 'fraud_detection_data_analysis'; database exists
 mysql> show databases;
